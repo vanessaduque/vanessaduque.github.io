978f45d0d799c388b8068796e08d78b2e72f0d06
Daniel Toro <toro.duque@gmail.com>
2021-02-15 21:52:25 +1100

    fix: public bundle

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,8 @@
 /* eslint-disable */
 
 const merge = require('webpack-merge');
-const path = require('path')
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    path: path.join(__dirname, ''),
-    filename: 'bundle.js'
-  }
+  mode: 'production'
 });
